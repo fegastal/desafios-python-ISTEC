ac7a7ae705a9ecd2ef3409057a5c270efc32a2f8
Fernanda Gastal <54810545+fegastal@users.noreply.github.com>
2022-01-08 17:32:11 +0000

    Add files via upload

---
+++ desafio1-webscrapper-wheater.py
@@ -0,0 +1,19 @@
+from requests_html import HTMLSession
+
+s = HTMLSession()
+
+query = 'lisbon'
+url = f'https://www.google.com/search?q=wheater+{query}'
+
+r = s.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'})
+
+temp = r.html.find('span#wob_tm', first=True).text
+unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
+desc = r.html.find('div.VQF4g', first=True).find('span#wob_dc', first=True).text
+
+if temp in range(0, 10):
+    print(f"Temperatura de {temp} em Lisboa. Utilização de um casaco quente!".format(temp))
+elif temp in range(11, 20):
+    print(f"Temperatura de {temp} em Lisboa. Utilização de um casaco leve!".format(temp))
+else:
+    print(f"Temperatura de {temp} em Lisboa. Não é preciso levar casaco!".format(temp))
